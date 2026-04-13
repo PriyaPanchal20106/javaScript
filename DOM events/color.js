55b281fd01eda4d6f5dbe69acee35e88078d19da
PriyaPanchal20106 <panchalpriya201@gmail.com>
2026-04-13 22:35:34 +0530

    learn about event Listener

--- DOM events/color.js
+++ DOM events/color.js
@@ -1,6 +1,7 @@
 let h3 = document.querySelector("h2")
 let generateColor = document.querySelector("button");
 let box = document.querySelector(".box");
+
 generateColor.addEventListener("click", function(){
     let red = Math.floor(Math.random() * 255);
     let green = Math.floor(Math.random() * 255);
---
+++ DOM events/script.js
@@ -0,0 +1,52 @@
+// let para= document.querySelector("p")
+// para.addEventListener("click",function (){
+//     console.log("para was clicked")
+// })  
+
+let box = document.querySelector(".box")
+box.addEventListener("mouseenter",function(){
+    console.log("you entered in box")
+}) 
+
+//This in Event Listener
+
+let btn = document.querySelector("button")
+btn.addEventListener("click",function(){
+    console.log(this)
+    console.log(this.innerText)
+})
+
+ let h1 = document.querySelector("h1")
+// h1.addEventListener("click",function(){
+//     console.log(this.innerText)
+//     this.style.color = 'blue'
+// })
+ let h3 = document.querySelector("h3")
+// h3.addEventListener("click",function(){
+//     console.log(this.innerText)
+//     this.style.color = 'Green'
+// })
+// let para = document.querySelector(".para")
+// para.addEventListener("click",function(){
+//     console.log(this.innerText)
+//     this.style.color = 'red'
+// }) 
+
+function changecolor(){
+    console.log(this.innerText)
+    this.style.color='blue'
+}
+h1.addEventListener("click",changecolor)
+h3.addEventListener("click",changecolor)
+
+let button = document.querySelector(".btn")
+let head = document.querySelector("h1")
+let h31 = document.querySelector("h3")
+let p = document.querySelector("p")
+button.addEventListener("click",color)
+ function color(){
+    head.style.color='blue'
+    h31.style.color='green'
+    p.style.color ='red'
+ }
+
