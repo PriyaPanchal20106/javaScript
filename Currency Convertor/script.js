3ca6fb95aa15a3aff8ab3282b35f96adc5a646f0
PriyaPanchal20106 <panchalpriya201@gmail.com>
2026-07-13 11:59:57 +0530

    final done

--- Currency Convertor/script.js
+++ Currency Convertor/script.js
@@ -1,10 +1,14 @@
-const BASE_URL =
-  " https://2024-03-06.currency-api.pages.dev/v1/currencies";
+const apiKey = "05995531946cbcda2b7c5670";
+
+const BASE_URL =  `https://v6.exchangerate-api.com/v6/${apiKey}`;
+
+
 
   const dropdown = document.querySelectorAll(".dropdown select");
   const btn = document.querySelector("form button")
   const fromcurr = document.querySelector(".from select")
   const tocurr = document.querySelector(".to select")
+  const msg = document.querySelector(".msg");
 
 //   for(codes in countryList){
 //     console.log(codes,countryList[codes])
@@ -45,10 +49,26 @@ const BASE_URL =
     if(amtval == "" || amtval <1){
         amtval = 1;
         amount.value ="1";
+        //const URL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromcurr.value}`;
 
     }
     //console.log(fromcurr.value , tocurr.value)
-    const URL = `${BASE_URL}/${fromcurr.value.toLowerCase()}/${tocurr.value.toLowerCase()}.json`
-    let response = await fetch(URL);
-    console.log(response);
+   // const URL = `${BASE_URL}/${fromcurr.value.toLowerCase()}/${tocurr.value.toLowerCase()}.json`
+    // let response = await fetch(URL);
+    // let data = await response.json();
+
+    
+    // console.log(data);
+
+
+    const requestUrl = `${BASE_URL}/latest/${fromcurr.value}`;
+console.log(requestUrl);
+
+let response = await fetch(requestUrl);
+let data = await response.json();
+
+let rate = data[tocurr.value.toLowerCase()];
+
+ let finalAmt = (amtval * rate);
+ msg.innerText = `${amtval} ${fromcurr.value}  = ${finalAmt} ${tocurr.value}`
   })
